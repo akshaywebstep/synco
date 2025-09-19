db339359995813cea87761c9ac5790400d2137d1
Akshay Kapil <kapilakshayofficial@gmail.com>
2025-09-19 16:16:24 +0530

    update - 1

---
+++ src/models/admin/payment/PaymentGroup.js
@@ -0,0 +1,35 @@
+// models/PaymentGroup.js
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../../../config/db");
+
+const PaymentGroup = sequelize.define(
+  "PaymentGroup",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: DataTypes.TEXT,
+    createdBy: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+  },
+
+  {
+    tableName: "payment_groups",
+    timestamps: true,
+  }
+);
+
+// ✅ Association
+PaymentGroup.associate = (models) => {
+  PaymentGroup.belongsToMany(models.PaymentPlan, {
+    through: models.PaymentGroupHasPlan,
+    foreignKey: "payment_group_id",
+    otherKey: "payment_plan_id",
+    as: "paymentPlans",
+  });
+};
+
+module.exports = PaymentGroup;
