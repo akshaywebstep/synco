d3f4655f40f83af2522543e182935463950c51e0
akshaywebstep <akshaywebstep@gmail.com>
2025-11-17 12:55:36 +0530

    update-02

--- src/services/admin/birthdayParty/booking/birthdayPartyBooking.js
+++ src/services/admin/birthdayParty/booking/birthdayPartyBooking.js
@@ -317,7 +317,7 @@ exports.createBirthdayPartyBooking = async (data) => {
                             .replace(/{{studentFirstName}}/g, firstStudent.studentFirstName || "")
                             .replace(/{{studentLastName}}/g, firstStudent.studentLastName || "")
                             .replace(/{{studentName}}/g, `${firstStudent.studentFirstName || ""} ${firstStudent.studentLastName || ""}`)
-                            .replace(/{{address}}/g, data.address || "")
+                           .replace(/{{address}}/g, booking?.address || booking?.location || data.address || "")
                             .replace(/{{age}}/g, firstStudent.age || "")
                             .replace(/{{gender}}/g, firstStudent.gender || "")
                             .replace(/{{relationChild}}/g, firstParent.relationChild || "")
