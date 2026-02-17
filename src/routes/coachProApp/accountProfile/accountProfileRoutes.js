0137d827222adabd473a7956088be0e617b67200
akshaywebstep <akshaywebstep@gmail.com>
2026-02-17 17:08:17 +0530

    upd

--- src/routes/coachProApp/accountProfile/accountProfileRoutes.js
+++ src/routes/coachProApp/accountProfile/accountProfileRoutes.js
@@ -12,3 +12,4 @@ router.get("/:id", authMiddleware, coachProfile.getAdminProfile);
 router.put("/update/:id", upload.single("profile"), authMiddleware, coachProfile.updateAdmin);
 
 module.exports = router;
+// dsad
